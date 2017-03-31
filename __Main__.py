f79d997e6b7c56213d1f954d1a62ed611000d8f0
nickmorris991 <nickmorris25@outlook.com>
2017-03-31 17:18:03 -0400

    search query
    I have the results now I need to sort through them in a helpful way and
    make suggestions

--- __Main__.py
+++ __Main__.py
@@ -11,7 +11,7 @@ import argparse
 import sys
 
 # flask imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from wtforms import StringField, validators
 from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired
@@ -37,7 +37,7 @@ SEARCH_PATH = '/v3/businesses/search'
 BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
 TOKEN_PATH = '/oauth2/token'
 GRANT_TYPE = 'client_credentials'
-SEARCH_LIMIT = 100
+SEARCH_LIMIT = 5
 
 
 def obtain_bearer_token(host, path):
@@ -67,6 +67,32 @@ def obtain_bearer_token(host, path):
     return bearer_token
 
 
+def request(host, path, bearer_token, url_params=None):
+    """Given a bearer token, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        bearer_token (str): OAuth bearer token, obtained using client_id and client_secret.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % bearer_token,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+
 def search(bearer_token, term, location):
     """Query the Search API by a search term and location.
     Args:
@@ -94,17 +120,27 @@ app.config['SECRET_KEY'] = 'DontTellAnyone'
 #object for wtforms
 class SearchForm(FlaskForm):
     Zipcode = StringField('5 Digit Zipcode:', validators=[InputRequired()])
-    Keyword = StringField('Search Keyword:', validators=[InputRequired()])
+    Keyword = StringField('Search Keywords:', validators=[InputRequired()])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     form = SearchForm()
     if form.validate_on_submit():
-        #obtain_bearer_token(API_HOST, TOKEN_PATH)
-        #search(bearer_token, form.Keyword.data, form.Zipcode.data)
-        return form.Zipcode.data + "   " + form.Keyword.data
+        return redirect(url_for('.queryResults'))
     return render_template('index.html', form=form)
+
+
+
+@app.route('/Results', methods=['GET', 'POST'])
+def queryResults():
+    form = SearchForm()
+    if form.validate_on_submit():
+        access_token = obtain_bearer_token(API_HOST, TOKEN_PATH)
+        test = search(access_token, form.Keyword.data, form.Zipcode.data)
+        print(test)
+        return "testing"
+    #return render_remplate('index.html', form=form)
 
 
 
