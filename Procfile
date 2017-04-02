1bfe7a17385dbdd665d2531aabbbfee5403d8325
nickmorris991 <nickmorris25@outlook.com>
2017-04-02 19:00:23 -0400

    $PORT

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python __Main__.py
+web: python __Main__.py $127.0.0.1:5000
--- __Main__.py
+++ __Main__.py
@@ -154,4 +154,4 @@ def queryResults():
 
 # start the server
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
